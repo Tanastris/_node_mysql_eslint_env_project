45460d6fb8aefd2daf171ed64238293cf8841a19
RokasV <vezikauskas.r@gmail.com>
2023-12-22 09:17:05 +0200

    done

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const dbConfig = {
-  host: process.env.DB_USER,
-  user: process.env.DB_PASS,
-  password: process.env.DB_HOST,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
 };
 console.log('dbConfig ===', dbConfig);
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
+const { dbConfig } = require('./config');
 
 const app = express();
 
@@ -17,6 +18,22 @@ app.get('/', (req, res) => {
   res.json('Hello World');
 });
 
+testConnection();
+// connect
+async function testConnection() {
+  let conn;
+  try {
+    conn = await mysql.createConnection(dbConfig);
+    await conn.query('SELECT * FROM posts LIMIT 1');
+    console.log('Succesfuly connected to mysql');
+  } catch (error) {
+    console.log('testConnection failed, did you start XAMPP mate???');
+    console.log(error);
+  } finally {
+    if (conn) conn.end();
+  }
+}
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server runing on http://localhost:${PORT}`);
 });
